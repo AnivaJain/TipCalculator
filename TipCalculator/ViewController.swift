9baec720247ddaa63809eb0140ad5e41b6675926
sambhav jain <shinkuj@yahoo.com>
2020-07-08 16:15:24 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Sambhav Jain on 7/8/20.
+//  Copyright © 2020 SH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
